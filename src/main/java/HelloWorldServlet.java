dff006bf76810231124beca18de71190956c0a73
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-06-02 15:13:55 -0500

    feat: learning how to make and use servlets

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType("text/ html");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        System.out.println("name parameter= " + name);
+        if (name == null) {
+            out.println("<strong> Hello </strong>");
+        } else {
+            out.println("<strong> Hello World </strong>");
+        }
     }
+
 }
