f86b188fde4bb9a7dee2546e95675b24beb1b179
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-06-09 15:42:35 -0500

    feat: restricted access to profile page, redirected if not logged in and redirected once logged out.

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -5,14 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //made sure user has to login in order to view profile
-        if(request.getSession().getAttribute("user") == null) {
+        request.getSession().invalidate();
         response.sendRedirect("/login");
-        return;
-        }
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
